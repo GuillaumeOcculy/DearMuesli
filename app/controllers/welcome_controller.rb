79487566bc7e4565bb99c36e36d0bc961696812c
GuillaumeOcculy <occuly.guillaume@gmail.com>
2014-04-28 09:37:29 +0200

    add user Model

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,20 @@
 class WelcomeController < ApplicationController
   def index
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	@user.save
+  	flash[:success] = "Email enregistré !"
+  	redirect_to root_path
+  end
+
+
+
+
+  private
+  def user_params
+    params.require(:user).permit(:email)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Dearmuesli::Application.routes.draw do
-  get 'welcome/index'
+  resources :welcome
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
