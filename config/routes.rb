71a24b7c957c8469f4d864f8062177c56ca058fb
GuillaumeOcculy <occuly.guillaume@gmail.com>
2014-04-28 10:24:16 +0200

    add resources users to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Dearmuesli::Application.routes.draw do
   resources :welcome
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
